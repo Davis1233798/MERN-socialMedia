f08b1fd405af2a954bcd4ab94b4428e177f2b8f1
clockChen0608 <n11023318@gemail.yuntech.edu.tw>
2022-09-25 06:03:20 +0000

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 
 const App = () => {
   return (
-    <div>App</div>
+   <Container maxidth="lg" color="inherit">
+    <AppBar>
+      <Typography variant="h2" align='center' > Memory</Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch' spacing={4}>
+            <Grid item xs ={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs ={12} sm={7}>
+              <Form />
+            </Grid>
+            <Grid item xs ={12} sm={7}>
+            </Grid>
+
+          </Grid>
+        </Container>
+      </Grow>
+   </Container>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ import postRoutes from './route/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes)
+app.use('/', postRoutes);
 
 app.use(bodyParser.json({ limint: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limint: "30mb", extended: true }));
 app.use(cors);
 
 const CONNECTION_URL = 'mongodb+srv://jkes6203:aA126182900@cluster0.ned2p.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT:${PORT}`)))
